b88080e7a8b4ae5f5f7f18ef9f276ed3f8109d2c
Marcos Zillig <marcos.zillig@gmail.com>
2017-10-02 13:13:49 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,46 @@
+
+var xmlhttp = new XMLHttpRequest();
+var filmesArray = ["tt0451279", "tt5013056", "tt3896198", "tt3469046", 
+					"tt3890160", "tt2316204", "tt2250912", "tt2406566", 
+					"tt2239822", "tt1648190", "tt3315342", "tt1856101"];
+var rand = filmesArray[Math.floor(Math.random() * filmesArray.length)];
+var url = "http://www.theimdbapi.org/api/movie?movie_id="+rand;
+
+xmlhttp.onreadystatechange = function(){
+	if(this.readyState == 4 && this.status == 200) {
+		var myObj = JSON.parse(this.responseText);					
+		var form = document.getElementById('form');	
+		// document.getElementById("demo").innerHTML = myObj.title;
+		$('.img').attr('src', myObj.poster.thumb); 
+		$('#btn-ajuda').click(function(){
+			$('.txt-modal').html(myObj.storyline);
+		});
+		var i =1;
+		$('#enviar').click(function(){
+			var diretor = myObj.director;
+			var resposta = $('#resposta-quiz').val();
+			if(i <= 10){
+				if(diretor === resposta){				
+					$('.pontuacao').append('<li class="acertou">' + i + '</li>');					
+					i++;				
+				} else{
+					$('.pontuacao').append('<li class="errou">' + i + '</li>');
+					i++;							
+				}
+			}else{
+				alert("Suas chances acabaram meu xovem")
+			}				
+		});
+		
+	}
+}
+
+
+
+xmlhttp.open("GET",url, true);
+xmlhttp.send();
+
+
+
+
+
